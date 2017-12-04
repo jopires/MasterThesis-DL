03d5da9e4ac15262f7dff116846cce1bb7222d19
Joao Pires <g1.jpires@gmail.com>
2017-12-04 22:56:09 +0000

    Add files via upload

---
+++ DL_fw_tests.py
@@ -0,0 +1,32 @@
+# created by jpires at 10-02-2017
+# used to test frameworks installed
+
+import theano as t
+print("Theano:", t.__version__) 
+
+import keras as k
+print("Keras:", k.__version__)
+
+import lasagne as l
+print("Lasagne:", l.__version__)
+
+import tensorflow as tf
+print("TensorFlow:", tf.__version__)
+
+import numpy as n
+print("Numpy:", n.__version__)
+
+import scipy as s
+print("Scipy:",s.__version__)
+
+import matplotlib as m
+print("matplotlib:", m.__version__)
+
+import sklearn as s
+print("sklearn:", s.__version__)
+
+import pandas as p
+print("pandas:", p.__version__)
+
+#2install: R, caffe, torch, opencv, cntk, mxnet, dl4j
+
